bad659dd3627fbfcd6978f563320f26042ce9d56
Caitlin Sheeran <caitlinsheeran1@gmail.com>
2025-02-13 00:58:28 -0600

    Added files

---
+++ RockPapeScissors/RockPapeScissors/Program.cs
@@ -0,0 +1,125 @@
+﻿// Caitlin Sheeran
+// 2/12/2025
+
+
+
+namespace RockPapeScissors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Count player scores
+            int player1Score = 0;
+            int player2Score = 0;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("    Welcome to Rock Paper Scissors!   ");
+            Console.WriteLine("--------------------------------------");
+
+
+            while (true)
+            {
+                // displays the players scores
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n--------------------------------------");
+                Console.WriteLine($"Player 1:\t{player1Score}\nPlayer 2:\t{player2Score}");
+                Console.WriteLine("--------------------------------------");
+
+                // gets the first player's scores
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Player 1: ");
+                string player1Answer = Console.ReadLine().ToUpper();
+
+                // gets the second player's scores
+                Console.Write("Player 2: ");
+                string player2Answer = Console.ReadLine().ToUpper();
+
+                RockPaperScissors game = new RockPaperScissors(player1Answer, player2Answer);
+
+                // Displays which player won and adds to their score
+                if (game.Game(player1Answer, player2Answer) == "Player 1")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\n--------------------------------------");
+                    Console.WriteLine("             Player 1 Won!            ");
+                    Console.WriteLine("--------------------------------------");
+                    player1Score += 1;
+                }
+                else if (game.Game(player1Answer, player2Answer) == "Player 2")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\n--------------------------------------");
+                    Console.WriteLine("             Player 2 Won!              ");
+                    Console.WriteLine("--------------------------------------\n");
+                    player2Score += 1;
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\n--------------------------------------");
+                    Console.WriteLine("            It was a draw!            ");
+                    Console.WriteLine("--------------------------------------");
+                }
+
+               
+            }
+        }
+        public class RockPaperScissors
+        {
+            //fields
+            private string player1 = "n/a";
+            private string player2 = "n/a";
+
+            // gets and sets
+            public string Player1
+            {
+                get { return this.player1; }
+                set { this.player1 = value; }
+            }
+            public string Player2
+            {
+                get { return this.player2; }
+                set { this.player2 = value; }
+            }
+
+            // constructors
+            public RockPaperScissors(string aPlayer1, string aPlayer2)
+            {
+                this.player1 = aPlayer1;
+                this.player2 = aPlayer2;
+            }
+
+            // Methods
+            public string Game(string aPlayer1, string aPlayer2)
+            {
+                // Checks what the player1's score is and compares it to the second players score
+                if (aPlayer1 == "ROCK")
+                {
+                    if (aPlayer2 == "ROCK") return "Draw";
+                    else if (aPlayer2 == "PAPER") return "Player 2";
+                    else if (aPlayer2 == "SCISSORS") return "Player 1";
+                }
+
+                else if (aPlayer1 == "PAPER")
+                {
+                    if (aPlayer2 == "ROCK") return "Player 1";
+                    else if (aPlayer2 == "PAPER") return "Draw";
+                    else if (aPlayer2 == "SCISSORS") return "Player 2";
+                }
+
+                else if (aPlayer1 == "scissors")
+                {
+                    if (aPlayer2 == "ROCK") return "Player 2";
+                    else if (aPlayer2 == "PAPER") return "Player 1";
+                    else if (aPlayer2 == "SCISSORS") return "Draw";
+                }
+                return "Invalid choice from one of the players";
+            }
+
+        }
+
+    }
+}
---
+++ ThePasswordValidator/ThePasswordValidator/Program.cs
@@ -0,0 +1,118 @@
+﻿// Caitlin Sheeran
+// 2/12/2025
+
+
+namespace ThePasswordValidator
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool passwordValid = false;
+
+            // Tells the user the requirements for a password
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Please select a password that is:");
+            Console.WriteLine("---------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Between 6 - 13 characters\nHas at least 1 Uppercase and 1 Lowercase letter\n1 Number\nNo Uppercase T\nNo Ammpersand (T)");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("---------------------------------------------");
+
+
+
+            while (passwordValid == false)
+            {
+                // Tells the user to write a password and then sends it to the class PasswordValidator
+                Console.ForegroundColor= ConsoleColor.White;
+                Console.Write("Enter your password: ");
+                string userAnswer = Console.ReadLine();
+                PasswordValidator newUser = new PasswordValidator(userAnswer);
+
+
+                // Checks if the password is valid or not valid. The while loop breaks if the password is valid
+                if (newUser.PasswordChecker(userAnswer))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("---------------------------------------------");
+                    Console.WriteLine("The password is valid!");
+                    Console.WriteLine("---------------------------------------------");
+                    passwordValid = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("---------------------------------------------");
+                    Console.WriteLine("Password is not valid. Try again");
+                    Console.WriteLine("---------------------------------------------");
+                }
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+    public class PasswordValidator
+    {
+        //fields
+        private string password = "n/a";
+
+        //gets and sets
+        public string Password
+        {
+            get { return this.password; }
+            set {
+                if (value.Length >= 6 && value.Length <= 13) this.password = value; 
+            }
+        }
+
+        // constructor
+        public PasswordValidator(string aPassword)
+        {
+            this.password = aPassword;
+        }
+
+        // Checks if the password is valid
+        public bool PasswordChecker(string aPassword)
+
+        {
+            // counters for checking the password
+            int lowerCaseCounter = 0;
+            int upperCaseCounter = 0;
+            int numberCounter = 0;
+
+
+            foreach (char c in aPassword)
+            {
+                if (c.ToString() != "T" && c.ToString() != "&") // Makes sure these characters are not in the password
+                {
+                    // adds to the variables each time a upper, lower, or number is read
+                    if (char.IsUpper(c)) upperCaseCounter++; 
+                    else if (char.IsLower(c)) lowerCaseCounter++;
+                    else if (char.IsDigit(c)) numberCounter++;
+                }
+                else return false;
+ 
+            }
+
+            // checks to make sure at least one of each requirement is met
+            if (lowerCaseCounter >= 1 && upperCaseCounter >= 1 && numberCounter >= 1)
+            {
+                return true;
+            }
+            else return false;
+
+        }
+
+
+
+
+
+    }
+}
